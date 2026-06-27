58b36febb9b5ac3829792b4eb710486361e4f0c0
hwanyseo <hwanyseo@cubrid.com>
2026-06-27 12:12:25 +0900

    - fixed script error (mv) - changed cancle message

--- build.bat
+++ build.bat
@@ -132,13 +132,13 @@ if not exist "%OS_IMAGE_DIR%" (
 
 if "%IMAGE_COPY_OPTION%"=="1" (
     if exist "%SHELL_DIR%make_image\targz_images\%COPY_IMAGE_TAR_GZ_FILE%" (
-        mv "%OS_IMAGE_DIR%%INSTALL_IMAGE_TAR_GZ_FILE%" "%OS_IMAGE_DIR%%INSTALL_IMAGE_TAR_GZ_FILE%".temp
+        move /y "%OS_IMAGE_DIR%%INSTALL_IMAGE_TAR_GZ_FILE%" "%OS_IMAGE_DIR%%INSTALL_IMAGE_TAR_GZ_FILE%".temp
         echo [INFO] Copy local image...
-        copy "%SHELL_DIR%make_image\targz_images\%COPY_IMAGE_TAR_GZ_FILE%" "%OS_IMAGE_DIR%%INSTALL_IMAGE_TAR_GZ_FILE%" >nul
+        copy /y "%SHELL_DIR%make_image\targz_images\%COPY_IMAGE_TAR_GZ_FILE%" "%OS_IMAGE_DIR%%INSTALL_IMAGE_TAR_GZ_FILE%" >nul
         if %errorLevel% neq 0 (
             echo [ERROR] Image copy fail. Please check ["%SHELL_DIR%make_image\targz_images\%COPY_IMAGE_TAR_GZ_FILE%"]
             echo [ERROR] If you do not need to copy the image, use the '-c 0' option. e.g.^) build.bat -c 0
-            mv "%OS_IMAGE_DIR%%INSTALL_IMAGE_TAR_GZ_FILE%".temp "%OS_IMAGE_DIR%%INSTALL_IMAGE_TAR_GZ_FILE%"
+            move /y "%OS_IMAGE_DIR%%INSTALL_IMAGE_TAR_GZ_FILE%".temp "%OS_IMAGE_DIR%%INSTALL_IMAGE_TAR_GZ_FILE%"
             pause
             exit /b 1
         ) else (
